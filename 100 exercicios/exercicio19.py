0354c257c47d37c3fd883899cb1e840871a1f154
MateusMacedo1 <127368179+MateusMacedo1@users.noreply.github.com>
2025-11-28 23:06:41 +0000

    Atualizar exercicios

---
+++ 100 exercicios/exercicio19.py
@@ -0,0 +1,10 @@
+l1 = float(input('Digite o comprimento de um lado do triangulo: '))
+l2 = float(input('Digite o comprimento de um lado do triangulo: '))
+l3 = float(input('Digite o comprimento de um lado do triangulo: '))
+
+if l1 == l2 == l3:
+    print('Seu triangulo é um equilátero')
+elif l1 > l2 == l3 or l1 < l2 == l3 or l1 == l2 < l3 or l1 == l2 > l3 or l2 > l1 == l3 or l2 < l1 == l3:
+    print('Seu triangulo é isósceles')
+else:
+    print('Seu triangulo é escaleno')
