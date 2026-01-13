5f93819204a836b3c46ce1aced1d0aec2fa590db
MateusMacedo1 <127368179+MateusMacedo1@users.noreply.github.com>
2026-01-13 21:03:45 +0000

    exerciciohj

---
+++ outros/tupla.py
@@ -0,0 +1,9 @@
+# Crie um programa que armazena os dados de um aluno em um dicionário. O dicionário deve guardar o nome, idade e o Curso.
+# Após o cadastro, o programa deve mostrar todas as informações de um aluno de forma organizada, exibindo o nome do campo e seu respectivo valor.
+
+DADOS = {}
+DADOS['Nome é: ']= (input('Digite o nome: '))
+DADOS['Idade é: ']= int(input('Digite a idade: '))
+DADOS['Curso é: ']= (input('Digite o Curso: '))
+
+print('NOME: '+ DADOS['Nome é: '])
---
+++ outros/tupla2.py
@@ -0,0 +1,4 @@
+# Crie um dicionário que represente um produto, contendo o nome, preço e quantidade de estoque;
+# Em seguida, o programa deve pedir ao usuário que informe qual informação deseja consultar(nome, preço ou quantidade) e mostrar o valor correspondente
+# Caso o campo informado não exista, o programa deve exibir uma mensagem informando que o campo é invalida.
+
