5f257203341af313ceffefeef277b14c661cd5a9
MateusMacedo1 <127368179+MateusMacedo1@users.noreply.github.com>
2026-01-27 18:04:07 +0000

    outro3

--- outros/minha/matriz_lição.py
+++ outros/minha/matriz_lição.py
@@ -1,4 +1,4 @@
-# Multipliquem as matrizes
+# Matrizes A e B
 A = [
     [1, 2, 3],
     [4, 5, 6]
@@ -10,6 +10,7 @@ B = [
     [11, 12]
 ]
 
+# Matrizes C e D
 C = [
     [13, 14, 15, 16, 17],
     [18, 19, 20, 21, 22]
@@ -23,6 +24,9 @@ D = [
     [31, 32]
 ]
 
+# -------------------------
+# Multiplicação A x B
+# -------------------------
 matriz = []
 
 for i in range(2):
@@ -32,9 +36,13 @@ for i in range(2):
         for k in range(3):
             soma += A[i][k] * B[k][j]
         linha.append(soma)
-    matriz.append(linha) 
+    matriz.append(linha)
 
-print(matriz)
+print("A x B =", matriz)
+
+# -------------------------
+# Multiplicação C x D
+# -------------------------
 matriz2 = []
 
 for i in range(2):
@@ -44,18 +52,19 @@ for i in range(2):
         for k in range(5):
             soma += C[i][k] * D[k][j]
         linha.append(soma)
-    matriz2.append(linha) 
+    matriz2.append(linha)
 
-print(matriz2)
+print("C x D =", matriz2)
 
+# -------------------------
+# Soma válida (A + A)
+# -------------------------
 matrizr = []
 
 for i in range(2):
     linha = []
-    for j in range(2):
-        soma = 0
-        soma += C[i][j] + D[i][j]
-        linha.append(soma)
-    matrizr.append(linha)    
+    for j in range(3):
+        linha.append(A[i][j] + A[i][j])
+    matrizr.append(linha)
 
-print(matrizr)
+print("A + A =", matrizr)
