b07f603806473adb6f6549b2d7b679e7ea105415
MateusMacedo1 <127368179+MateusMacedo1@users.noreply.github.com>
2026-01-27 18:09:31 +0000

    atividade matriz

---
+++ atividade_matriz/10atividadeMatriz.py
@@ -0,0 +1,17 @@
+#CRIE UMA MATRIZ QUE REPRESENTE A QUANTIDADE DE PRODUTOS VENDIDOS (2 × 2)
+# E OUTRA MATRIZ QUE REPRESENTE O PREÇO DESSES PRODUTOS (2 × 2).
+#UTILIZE A MULTIPLICAÇÃO DE MATRIZES PARA GERAR UMA MATRIZ QUE REPRESENTE O VALOR TOTAL DAS VENDAS.
+PRODUTOS =[
+    [1, 2, 3],
+    [4, 5, 6]
+]
+PREÇO = [
+        [1, 2],
+        [3, 4],
+        [5, 6]
+]
+
+for i in range(2):
+    for j in range(2):
+        PRODUTOS[i][j] *= PREÇO[i][j]
+    print(PRODUTOS[i])
