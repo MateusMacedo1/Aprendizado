4ffd0a5848aa98cb130ca900989ab9b63aa2a1c9
MateusMacedo1 <127368179+MateusMacedo1@users.noreply.github.com>
2026-02-10 18:08:49 +0000

    importar

--- biblioteca/ex5.py
+++ biblioteca/ex5.py
@@ -1,4 +1,7 @@
 #Crie um programa que importe a biblioteca datetime e mostre a data atual no formato dia/mês/ano.
 
-import datetime
-print(datetime.datetime(day= 10, month = 2, year= 2026))
+from datetime import date
+hoje = date.today()
+
+data_br = hoje.strftime("%d/%m/%Y")
+print(data_br)
